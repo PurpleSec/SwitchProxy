2099fd09108760bbb7b2d83f90a9f3b267bc7162
iDigitalFlame <idf@idigitalflame.com>
2019-08-15 12:03:59 -0400

    Initial commit

---
+++ switchproxy/go.mod
@@ -0,0 +1,3 @@
+module github.com/iDigitalFlame/switchproxy
+
+go 1.12
---
+++ switchproxy/proxy.go
@@ -0,0 +1,110 @@
+package switchproxy
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"sync"
+)
+
+// Proxy is a struct that repersents a stacked proxy that allows a forwarding proxy
+// with secondary read only Switch connections that allow logging and storing the connection data.
+type Proxy struct {
+	key       string
+	cert      string
+	pool      *sync.Pool
+	server    *http.Server
+	primary   *Switch
+	secondary []*Switch
+}
+
+// Start starts the Server listening loop and returns an error if the server could not be started.
+// Only returns an error if any IO issues occur during operation.
+func (p *Proxy) Start() error {
+	if len(p.cert) > 0 && len(p.key) > 0 {
+		return p.server.ListenAndServeTLS(p.cert, p.key)
+	}
+	return p.server.ListenAndServe()
+}
+
+// Primary sets the primary Proxy Switch context.
+func (p *Proxy) Primary(s *Switch) {
+	p.primary = s
+}
+
+// NewProxy creates a new Proxy struct from the supplied options.
+func NewProxy(listen string) *Proxy {
+	return NewProxyEx(listen, "", "")
+}
+func (p *Proxy) putClear(b *bytes.Buffer) {
+	b.Reset()
+	p.pool.Put(b)
+}
+
+// AddSecondary adds an additional one-way Switch context.
+func (p *Proxy) AddSecondary(s ...*Switch) {
+	p.secondary = append(p.secondary, s...)
+}
+
+// NewProxyEx creates a new Proxy struct from the supplied options.
+// This function allows fos specifying TLS options.
+func NewProxyEx(listen, cert, key string) *Proxy {
+	p := &Proxy{
+		key:  key,
+		cert: cert,
+		pool: &sync.Pool{
+			New: func() interface{} {
+				return new(bytes.Buffer)
+			},
+		},
+		server: &http.Server{
+			Addr:    listen,
+			Handler: &http.ServeMux{},
+		},
+		secondary: make([]*Switch, 0),
+	}
+	p.server.Handler.(*http.ServeMux).Handle("/", p)
+	return p
+}
+
+// ServeHTTP satisifies the http.Hanndler interface.
+func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	i := p.pool.Get().(*bytes.Buffer)
+	defer p.putClear(i)
+	defer r.Body.Close()
+	if _, err := io.Copy(i, r.Body); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, http.StatusText(http.StatusInternalServerError))
+		return
+	}
+	o := p.pool.Get().(*bytes.Buffer)
+	defer p.putClear(o)
+	for n := range p.secondary {
+		p.secondary[n].process(r, i, o)
+		o.Reset()
+	}
+	if p.primary != nil {
+		c, h, err := p.primary.process(r, i, o)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, http.StatusText(http.StatusInternalServerError))
+			fmt.Fprintf(w, err.Error())
+			return
+		}
+		for k, v := range h {
+			w.Header()[k] = v
+		}
+		w.WriteHeader(c)
+		if _, err := io.Copy(w, o); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, http.StatusText(http.StatusInternalServerError))
+			fmt.Fprintf(w, err.Error())
+			return
+		}
+	} else {
+		w.WriteHeader(http.StatusServiceUnavailable)
+		fmt.Fprintf(w, http.StatusText(http.StatusServiceUnavailable))
+		return
+	}
+}
